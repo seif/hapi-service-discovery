3e07950b3d8c1df156965ea2e8ed0b4639a1df92
Andy Royle <ajroyle@gmail.com>
2014-08-22 10:42:43 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+var service = require('./lib/service'),
+    config = {};
+
+exports.register = function(plugin, options, next){
+  config = options;
+
+  plugin.log(["discovery"], "registering discovery routes");
+  plugin.route([
+    {
+      method: "GET",
+      path: "/discovery/announce",
+      config: {
+        handler: function(request, reply){
+          service.announce(function(err){
+            if(err){
+                return reply(err);
+            }
+
+            reply();
+          });
+        }
+      }
+    },
+    {
+      method: "GET",
+      path: "/discovery/unannounce",
+      config: {
+        handler: function(request, reply){
+          service.unannounce(function(err){
+            if(err){
+              return reply(err);
+            }
+            reply();
+          });
+        }
+      }
+    },
+    {
+      method: "GET",
+      path: "/discovery/lease",
+      config: {
+        handler: function(request, reply){
+          var lease = discovery.lease();
+          if(!lease){
+            return reply().code(404);
+          }
+
+          reply(lease);
+        }
+      }
+    },
+    {
+      method: "GET",
+      path: "/discovery/lastUpdate",
+      config: {
+        handler: function(request, reply){
+          reply({ lastUpdate: discovery.lastUpdate().toISOString() });
+        }
+      }
+    }
+  ]);
+
+  discovery.init(plugin, config, function(){
+    next();
+  });
+};
+
+exports.register.attributes = {
+  pkg: require('./package.json')
+};
